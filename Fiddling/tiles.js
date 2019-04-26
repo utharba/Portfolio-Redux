95679a11cf65696be015dc40d3040bbd3e9ae4ec
Matt Simon <msimon363@gmail.com>
2019-04-26 16:58:56 -0700

    modified:   Fiddling/tiles.css 	modified:   Fiddling/tiles.html 	modified:   Fiddling/tiles.js 	new file:   Portfolio/main.css 	new file:   Portfolio/main.html

--- Fiddling/tiles.js
+++ Fiddling/tiles.js
@@ -1,4 +1,20 @@
-function tileMove (pos1,pos2,pos3,posBlank,tiles) {
+function moveTile1 (tile,blankTile) {
+
+    var tile = document.getElementById("pos1").childNodes;
+    //tile1
+    var blankTile = document.getElementById("pos4").childNodes;
+
+    document.getElementById("pos1").replaceChild(blankTile,tile);
+    document.getElementById("pos4").replaceChild(tile,blankTile);
+
+    var i;
+ //   for (i = 0; i < x)
+    
+};
+console.log(document.getElementById("tile1"));
+console.log(document.getElementById("tile4"));
+/*
+function moveTile2 (pos1,pos2,pos3,posBlank,tiles) {
     var pos1 = "#pos1";
     var pos2 = "#pos2";
     var pos3 = "#pos3";
@@ -6,12 +22,38 @@ function tileMove (pos1,pos2,pos3,posBlank,tiles) {
 
     var tileList = document.getElementsByClassName(".tile");
     
-    
+    if 
 
     var i;
     for (i = 0; i < x)
     
 }
-document.getElementsByClassName("tile").addEventListener("onclick", tileMove);
+function moveTile1 (pos1,pos2,pos3,posBlank,tiles) {
+    var pos1 = "#pos1";
+    var pos2 = "#pos2";
+    var pos3 = "#pos3";
+    var posBlank = "#pos4";
 
-console.log(document.getElementsByClassName("tile"))
+    var tileList = document.getElementsByClassName(".tile");
+    
+    if 
+
+    var i;
+    for (i = 0; i < x)
+    
+}
+function moveTile1 (pos1,pos2,pos3,posBlank,tiles) {
+    var pos1 = "#pos1";
+    var pos2 = "#pos2";
+    var pos3 = "#pos3";
+    var posBlank = "#pos4";
+
+    var tileList = document.getElementsByClassName(".tile");
+    
+    if 
+
+    var i;
+    for (i = 0; i < x)
+    
+}*/
+console.log(document.getElementsByClassName("tile"));
